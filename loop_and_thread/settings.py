21ee0898ecde979cb6f3438f941a5c763332c4a3
chloejones9464 <jones.chloe@live.co.uk>
2025-10-06 17:38:48 +0100

    Added cache control

--- loop_and_thread/settings.py
+++ loop_and_thread/settings.py
@@ -194,6 +194,12 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 if 'USE_AWS' in os.environ:
+    # Cache control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=86400',
+    }
+
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'loopandthread'
